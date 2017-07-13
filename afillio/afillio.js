3a08c55df1ec160dd3205c45753a047aabef76bd
edrcq <erucquoy@student.42.fr>
2017-07-13 17:29:29 +0200

    email swal test

---
+++ afillio/afillio.js
@@ -0,0 +1,30 @@
+var afillio = {};
+
+document.addEventListener("DOMContentLoaded", afillioCore);
+
+afillio.affilioShowBox = () => {
+	swal({
+		title: 'Input email address',
+		input: 'email'
+		}).then(function (email) {
+			swal({
+				type: 'success',
+				html: 'Entered email: ' + email
+			})
+	})
+};
+
+function afillioCore() {
+	var pluginDiv = document.createElement('div');
+
+	pluginDiv.style.cssText = 'position: absolute; right: 0px; top: 100px; background-color: blue;color: white;padding-top: 10px;padding-left: 10px;padding-bottom: 10px;padding-right: 10px;';
+
+	pluginDiv.innerHTML = 'Content (click for test)';
+
+	document.body.appendChild(pluginDiv);
+
+	pluginDiv.addEventListener("click", affilioShowBox, false);
+}
+
+
+
