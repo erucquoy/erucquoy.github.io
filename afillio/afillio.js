6f525428c7cea9ad2752a5afecd41f48b0da6765
edrcq <erucquoy@student.42.fr>
2017-07-13 17:33:50 +0200

    fix

--- afillio/afillio.js
+++ afillio/afillio.js
@@ -1,8 +1,4 @@
-var afillio = {};
-
-document.addEventListener("DOMContentLoaded", afillioCore);
-
-afillio.affilioShowBox = () => {
+function affilioShowBox() {
 	swal({
 		title: 'Input email address',
 		input: 'email'
@@ -14,17 +10,14 @@ afillio.affilioShowBox = () => {
 	})
 };
 
-function afillioCore() {
-	var pluginDiv = document.createElement('div');
+var pluginDiv = document.createElement('div');
 
-	pluginDiv.style.cssText = 'position: absolute; right: 0px; top: 100px; background-color: blue;color: white;padding-top: 10px;padding-left: 10px;padding-bottom: 10px;padding-right: 10px;';
+pluginDiv.style.cssText = 'position: absolute; right: 0px; top: 100px; background-color: blue;color: white;padding-top: 10px;padding-left: 10px;padding-bottom: 10px;padding-right: 10px;';
 
-	pluginDiv.innerHTML = 'Content (click for test)';
+pluginDiv.innerHTML = 'Content (click for test)';
 
-	document.body.appendChild(pluginDiv);
+document.body.appendChild(pluginDiv);
 
-	pluginDiv.addEventListener("click", afillio.affilioShowBox, false);
-}
 
 
 
